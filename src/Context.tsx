2ff37216fcf702a51066a9087c8adab66e857d91
Thailan Gouvea <thailangm@icloud.com>
2024-11-18 16:05:35 +0200

    Adds both alert and snackbar components

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,3 +1,4 @@
+import { Alert, Snackbar } from "@mui/material";
 import { createContext, useContext, useState } from "react";
 
 interface AppProviderProps {
@@ -8,22 +9,51 @@ interface AppContextInterface {}
 
 const AppContext = createContext<AppContextInterface | null>(null);
 
-const mudarIdioma = () => {};
-
-const showSnackMessage = (message) => {
-  console.log(message);
-};
-
-const [snackOpen, setSnackOpen] = useState(false);
-
 const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
+  const [snackOpen, setSnackOpen] = useState(false);
+  const [snackMessage, setSnackMessage] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertSeverity, setAlertSeverity] = useState("");
+  const timeOutDuration = 6000;
+
+  const showSnackMessage = (message: string) => {
+    setSnackMessage("Cliquei");
+    setSnackOpen(true);
+
+    setTimeout(() => {
+      setSnackOpen(false);
+      setSnackMessage("");
+    }, 5000);
+  };
+
+  const handleClose = () => {
+    setSnackOpen(false);
+    setSnackMessage("");
+  };
+
+  const showAlertMessage = (message, severity) => {
+    setAlertMessage(message);
+    setAlertSeverity(severity);
+
+    setTimeout(() => {
+      setAlertMessage("");
+      setAlertSeverity("");
+    }, 4000);
+  };
+  const mudarIdioma = () => {};
+
   const sharedState = {
     mudarIdioma,
     showSnackMessage,
+    showAlertMessage,
   };
 
   return (
-    <AppContext.Provider value={sharedState}>{children}</AppContext.Provider>
+    <AppContext.Provider value={sharedState}>
+      {children}
+      <Snackbar message={snackMessage} open={snackOpen} onClose={handleClose} />
+      <Alert severity={alertSeverity}>{alertMessage}</Alert>
+    </AppContext.Provider>
   );
 };
 
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,8 @@
 import { Button } from "@mui/material";
-import { useContext } from "react";
 import { useAppContext } from "../Context";
 
 export default function Home() {
-  const { showSnackMessage } = useAppContext();
-
-  showSnackMessage("delicia");
+  const { showSnackMessage, showAlertMessage } = useAppContext();
 
   return "Home";
 }
